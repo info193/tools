4ea3351bfe5ab85d6828c2c203bb433d6e94ccd4
huguopeng <ghupeng@163.com>
2024-04-26 21:23:34 +0800

    add

--- test/trim_time_test.go
+++ test/trim_time_test.go
@@ -6,46 +6,40 @@ import (
 	"testing"
 )
 
-func TestTimeSpan(t *testing.T) {
-	//current := time.Now()
-	//c, _ := time.ParseInLocation("2006-01-02 15:04", "2024-04-06 10:20", time.Local)
-	//if current.After(c) {
-	//	fmt.Println("当前时间大于结束时间")
-	//}
-	//
-	//if c.Before(current) {
-	//	fmt.Println("结束时间大于当前时间")
-	//}
+func TestTrimTime(t *testing.T) {
 
 	var duration int64 = 60
-	periodStartHour := "20:00"
-	periodEndHour := "22:30"
-	//subscribeStartDate := "2024-04-09 23:11"
-	//subscribeEndDate := "2024-04-10 01:00"
-	//trimTime := utils.NewTrimTime(duration, periodStartHour, periodEndHour, subscribeStartDate, subscribeEndDate)
-	//ts := trimTime.Period()
-	//fmt.Println(fmt.Sprintf("%+v", ts))
+	periodStartHour := "12:00"
+	periodEndHour := "01:00"
+	// 隔日 2
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-23 05:02", "2024-04-23 07:00")
 
-	//trimTime := utils.NewTrimTime(duration, periodStartHour, periodEndHour, "2024-04-23 18:25", "2024-04-23 22:25")
-	//coupon := trimTime.Period()
-	//fmt.Println(coupon)
+	// 隔日  0000000000
+	trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-21 00:30", "2024-04-22 10:10")
 
-	trimTime := utils.NewTrimTime(duration, periodStartHour, periodEndHour, "2024-04-23 18:57", "2024-04-23 20:57")
-	coupon := trimTime.Period()
-	fmt.Println(coupon)
+	//隔日 开始时段 12:00 - 04:00  预约时间2024-04-06 23:59 - 2024-04-07 02:30  2024-04-06 23:59 - 2024-04-07 02:30
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-22 23:00", "2024-04-23 02:55")
 
-	//fmt.Println(trimTime.Period())
-	//boundaryDuration := trimTime.Neutron * 60 // 边界时间
-	//if trimTime.EndTime.Unix()-trimTime.StartTime.Unix() < boundaryDuration {
-	//	fmt.Println(fmt.Sprintf("设定开始时间及结束时间小于%v分钟", trimTime.Neutron))
-	//	return
-	//}
-	//fmt.Println(trimTime.Period(), "======")
-	//给定优惠券时段限制有两种情况：
-	//第一种情况跨天时段：开始时段12:00、结束时段05:00
-	//第二种情况每日时段：开始时段08:00、结束时段19:00
-	//请帮我写一个需求，使用golang语言，
-	//给定优惠券时段限制开始时段08:00、结束时段19:00
-	//检测用户预约开始时间及结束时间，并且用户选择的优惠券时长1小时，请判断优惠券是否可用，并算出优惠券在用户预约开始时间及结束时间那个时间点使用，在可用或不可用的情况下都给出可用的时间点。
-	//用户预约时间可用预约好几天，需检测预约时间段内优惠券是否可用且算出优惠券在最近的某一天预约的时间内使用，并且在可用或不可用的情况下都给出可用的时间范围。
+	// 隔日 开始时段 12:00 - 02:00  预约时间2024-04-06 23:59 - 2024-04-07 01:30  时长180
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-22 23:59", "2024-04-23 01:30")
+
+	// 今日 开始时段 02:00 - 12:00  预约时间2024-04-22 06:20 - 2024-04-22 12:10  时长180   返回 111111111
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-22 06:20", "2024-04-22 12:10")
+
+	// 今日 开始时段 02:00 - 12:00  预约时间2024-04-22 06:20 - 2024-04-22 09:10  时长180   返回 22222222222
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-22 06:20", "2024-04-22 09:10")
+
+	// 今日 开始时段 02:00 - 12:00  预约时间2024-04-22 06:20 - 2024-04-22 09:50  时长180   返回 3333333333
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-22 06:20", "2024-04-22 09:50")
+
+	//// 今日 开始时段 02:00 - 12:00  预约时间2024-04-22 00:20 - 2024-04-22 02:00  时长180   返回 0000000000 不可使用
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-22 00:20", "2024-04-22 02:00")
+
+	// 今日 开始时段 02:00 - 12:00  预约时间2024-04-22 00:20 - 2024-04-22 05:00  时长180   返回 0000000000 不可使用
+	//trimTime := utils.NewTrimCardTime(duration, periodStartHour, periodEndHour, "2024-04-22 02:00", "2024-04-22 18:35")
+
+	//coupon := trimTime.CardPeriod()
+	coupon := trimTime.CouponPeriod(utils.OptimalYes)
+	fmt.Println(fmt.Sprintf("%+v", coupon))
+
 }
