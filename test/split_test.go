b48532b733c77756059a338dd0871709800663e0
muyu <info193@163.com>
2024-10-26 21:51:29 +0800

    add

--- test/split_test.go
+++ test/split_test.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"github.com/info193/tools/utils"
 	"testing"
+	"time"
 )
 
 func TestSplitStr64(t *testing.T) {
 	fmt.Println(utils.SplitStr64([]int64{}))
 	fmt.Println(utils.SplitStr([]int{1, 2, 3}))
+	fmt.Println(1718363100 - time.Now().Unix())
 }
-
